c89d02fb267797ad9513e1b9c9776ad600917674
Darrion Gering and Stuart McKay <darrion+stuart@>
2019-06-18 16:40:37 -0700

    initialize firebase, needs new api key to run

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,17 @@ import { routing } from './app-routing.module';
 import { AboutComponent } from './about/about.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { FarmerComponent } from './farmer/farmer.component';
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
