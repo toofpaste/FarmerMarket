7212719e7958518b14b6da71ec678eebcf554cfd
Stuart McKay and michael larragueta <stuart+michael@>
2019-06-19 15:56:46 -0700

    link firebase and import farmer.json

--- src/app/farmer.service.ts
+++ src/app/farmer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Farmer } from './farmer/farmer.model';
 import { FARMERS } from './farmer/mock-farmers';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class FarmerService {
